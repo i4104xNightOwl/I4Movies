623d74e2f2509cb1ed1b5a6ae1bfe649012e1804
I4104 Night Owl <I4104.vn@gmail.com>
2025-07-09 11:00:52 +0700

    FEAT(Header): Tạo header và sửa lại css cho hợp lý

--- frontend/apps/movies/src/app/main/app.ts
+++ frontend/apps/movies/src/app/main/app.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FooterComponent } from "../shared/footer/footer.component";
+import { FooterComponent } from "../shared/layouts/footer/footer.component";
+import { HeaderComponent } from '../shared/layouts/header/header.component';
 
 @Component({
-  imports: [RouterModule, FooterComponent],
-  selector: 'app-root',
-  templateUrl: './app.html',
-  styleUrl: './app.scss',
+    imports: [RouterModule, FooterComponent, HeaderComponent],
+    selector: 'app-root',
+    templateUrl: './app.html',
+    styleUrl: './app.scss',
 })
 
 export class App {
-  protected title = 'movies';
+    protected title = 'movies';
 }
