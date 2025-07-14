f6b12356b5370d3f8bae9415fc62410b4592cfe8
I4104 Night Owl <I4104.vn@gmail.com>
2025-07-14 16:38:14 +0700

    FEAT(Service): Tạo categories service và viết bài test

---
+++ packages/backend/src/services/mongodb/categories.services.ts
@@ -0,0 +1,20 @@
+import { ICategoriesService } from '@interface/services/categories.services';
+import { ICategories } from '@movies/interfaces';
+
+export default class CategoriesService implements ICategoriesService {
+    get(id: string): Promise<ICategories> {
+        throw new Error('Method not implemented.');
+    }
+    getAll(): Promise<ICategories> {
+        throw new Error('Method not implemented.');
+    }
+    create(data: ICategories): Promise<ICategories> {
+        throw new Error('Method not implemented.');
+    }
+    update(data: ICategories): Promise<ICategories> {
+        throw new Error('Method not implemented.');
+    }
+    delete(data: ICategories): Promise<boolean> {
+        throw new Error('Method not implemented.');
+    }
+}
---
+++ packages/backend/tests/services/categories.service.test.ts
@@ -0,0 +1,26 @@
+import { ICategories, IMovies } from '@movies/interfaces';
+import Database from '../../utils/database';
+import CategoriesService from '@src/services/mongodb/categories.services';
+
+let defaultCategories: ICategories = {
+    name: 'Action',
+    slug: 'action'
+}
+
+beforeAll(async () => {
+    await Database.getInstance();
+});
+
+afterAll(async () => {
+    await Database.disconnect();
+});
+
+describe('MoviesService', () => {
+    const categoriesService = new CategoriesService();
+
+    it('Tạo mới một categories', async () => {
+        const categories = await categoriesService.create(defaultCategories);
+        expect(categories.name).toBe(defaultCategories.name);
+        expect(categories.slug).toBe(defaultCategories.slug);
+    });
+});
